69a787f44b546bdcec6bb6a6c977a99e8f39718e
Brian Toth <btoth@palantir.com>
2015-07-27 11:55:47 -0700

    Turned off default exception mappers.

--- magic-server/src/main/java/magic/exceptions/CatchAllExceptionMapper.java
+++ magic-server/src/main/java/magic/exceptions/CatchAllExceptionMapper.java
@@ -2,7 +2,7 @@ package magic.exceptions;
 
 import javax.ws.rs.core.Response.Status;
 
-public class CatchAllExceptionMapper extends AbstractExceptionMapper<Exception> {
+public class CatchAllExceptionMapper extends AbstractExceptionMapper<Throwable> {
 
     public CatchAllExceptionMapper() {
         super(Status.INTERNAL_SERVER_ERROR);
