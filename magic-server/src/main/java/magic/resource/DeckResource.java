bd03daa93df45972ca0062f047772a72a7c5e754
billbindi <billdbindi@gmail.com>
2015-08-16 13:54:24 -0700

    Reading capabilities to database.

--- magic-server/src/main/java/magic/resource/DeckResource.java
+++ magic-server/src/main/java/magic/resource/DeckResource.java
@@ -1,7 +1,12 @@
 package magic.resource;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import magic.data.Deck;
 import magic.data.database.Database;
@@ -13,5 +18,17 @@ public class DeckResource {
     @Path("/register")
     public void registerDeck(Deck deck) {
         Database.addDeck(deck);
+    }
+
+    @GET
+    @Path("/list")
+    public List<Deck> listDecks() throws IOException {
+        return Database.readDecks();
+    }
+
+    @GET
+    @Path("/list/{id}")
+    public Deck listDeck(@PathParam("id") Long id) throws IOException {
+        return Database.readDeckFromId(id);
     }
 }
