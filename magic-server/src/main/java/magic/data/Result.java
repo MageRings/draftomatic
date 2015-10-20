5f53404acfa932d71703130a833d5c8ddc21fb22
Brian Toth <bri.toth@gmail.com>
2015-10-20 14:10:21 -0700

    Added checks to make sure that input results are valid.  Fixed issue with total points in pairings.

--- magic-server/src/main/java/magic/data/Result.java
+++ magic-server/src/main/java/magic/data/Result.java
@@ -6,9 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Result implements Comparable<Result> {
 
     public static enum Outcome {
-        P1_WIN,
-        P1_LOSS,
-        DRAW
+        P1_WIN, P1_LOSS, DRAW
     }
 
     public static final Result INCOMPLETE = new Result(0, 0, 0);
@@ -26,24 +24,23 @@ public class Result implements Comparable<Result> {
     }
 
     public int getP1Wins() {
-        return p1Wins;
+        return this.p1Wins;
     }
 
     public int getP2Wins() {
-        return p2Wins;
+        return this.p2Wins;
     }
 
     public int getDraws() {
-        return draws;
+        return this.draws;
     }
-
 
     @JsonIgnore
     public Outcome determineOutcome() {
-        if (p1Wins > p2Wins) {
+        if (this.p1Wins > this.p2Wins) {
             return Outcome.P1_WIN;
         }
-        if (p2Wins > p1Wins) {
+        if (this.p2Wins > this.p1Wins) {
             return Outcome.P1_LOSS;
         }
         return Outcome.DRAW;
@@ -51,22 +48,22 @@ public class Result implements Comparable<Result> {
 
     @JsonIgnore
     public int getNumberOfGames() {
-        return p1Wins + p2Wins + draws;
+        return this.p1Wins + this.p2Wins + this.draws;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + draws;
-        result = prime * result + p1Wins;
-        result = prime * result + p2Wins;
+        result = prime * result + this.draws;
+        result = prime * result + this.p1Wins;
+        result = prime * result + this.p2Wins;
         return result;
     }
 
     @Override
     public String toString() {
-        return "Result [p1Wins=" + p1Wins + ", p2Wins=" + p2Wins + ", draws=" + draws + "]";
+        return "Result [p1Wins=" + this.p1Wins + ", p2Wins=" + this.p2Wins + ", draws=" + this.draws + "]";
     }
 
     @Override
@@ -81,13 +78,13 @@ public class Result implements Comparable<Result> {
             return false;
         }
         Result other = (Result) obj;
-        if (draws != other.draws) {
+        if (this.draws != other.draws) {
             return false;
         }
-        if (p1Wins != other.p1Wins) {
+        if (this.p1Wins != other.p1Wins) {
             return false;
         }
-        if (p2Wins != other.p2Wins) {
+        if (this.p2Wins != other.p2Wins) {
             return false;
         }
         return true;
@@ -98,13 +95,13 @@ public class Result implements Comparable<Result> {
         if (o == null) {
             return 1;
         }
-        if (p1Wins != o.p1Wins) {
-            return Integer.compare(p1Wins, o.p1Wins);
+        if (this.p1Wins != o.p1Wins) {
+            return Integer.compare(this.p1Wins, o.p1Wins);
         }
-        if (p2Wins != o.p2Wins) {
-            return Integer.compare(p2Wins, o.p2Wins);
+        if (this.p2Wins != o.p2Wins) {
+            return Integer.compare(this.p2Wins, o.p2Wins);
         }
-        return Integer.compare(draws, o.draws);
+        return Integer.compare(this.draws, o.draws);
     }
 
 }
---
+++ magic-server/src/test/java/magic/tournament/RegisterRoundTests.java
@@ -0,0 +1,119 @@
+package magic.tournament;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.NavigableSet;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import magic.data.Format;
+import magic.data.Match;
+import magic.data.Pairing;
+import magic.data.Player;
+import magic.data.Result;
+import magic.data.Round;
+import magic.data.tournament.TournamentData;
+import magic.data.tournament.TournamentInput;
+
+@RunWith(Parameterized.class)
+public final class RegisterRoundTests {
+
+    private static final String TOURNAMENT_ID    = "id";
+    private static final int    NUMBER_OF_ROUNDS = 3;
+
+    private static final Player MIKE     = new Player(1, "Mike");
+    private static final Player KIMBERLY = new Player(2, "Kimberly");
+
+    // total points are not relevant to tiebreaker calculations, only for sorting pairings
+    private static final Pairing p1 = new Pairing(MIKE, KIMBERLY, 0);
+
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {
+                        "Test tournament complete",
+                        ImmutableSet.of(),
+                        ImmutableSortedSet.of(
+                                new Round(1, true, Sets.newTreeSet()),
+                                new Round(2, true, Sets.newTreeSet()),
+                                new Round(3, true, Sets.newTreeSet())),
+                        Optional.of(3),
+                        null,
+                        "This tournament is already compelete!"
+                },
+                {
+                        "Test round not expected",
+                        ImmutableSet.of(),
+                        ImmutableSortedSet.of(
+                                new Round(1, true, Sets.newTreeSet()),
+                                new Round(2, true, Sets.newTreeSet())),
+                        Optional.of(1),
+                        null,
+                        "You may only enter results for the current round (2)!",
+                },
+                {
+                        "Test illegal wins",
+                        ImmutableSet.of(MIKE, KIMBERLY),
+                        Sets.newTreeSet(Lists.newArrayList(
+                                new Round(1, false, Sets.newTreeSet(ImmutableSet.of(
+                                        new Match(p1, Result.INCOMPLETE, false, false)))))),
+                        Optional.empty(),
+                        Sets.newTreeSet(ImmutableSet.of(
+                                new Match(p1, new Result(3, 1, 0), false, false))),
+                        "Match Match [pairing=Pairing [player1=Player [id=1, name=Mike], player2=Player" +
+                                " [id=2, name=Kimberly], totalPoints=0], result=Result [p1Wins=3, p2Wins=1, draws=0]," +
+                                " p1Drop=false, p2Drop=false] is invalid." +
+                                "  Matches are played until one player has two wins."
+                },
+        });
+    }
+
+    private final Set<Player>         players;
+    private final NavigableSet<Round> rounds;
+    private final Optional<Integer>   roundRequested;
+    private final Collection<Match>   roundResults;
+    private final String              errorExpected;
+
+    public RegisterRoundTests(String testName,
+                              Set<Player> players,
+                              NavigableSet<Round> rounds,
+                              Optional<Integer> roundRequested,
+                              Collection<Match> roundResults,
+                              String errorExpected) {
+        this.players = players;
+        this.rounds = rounds;
+        this.roundRequested = roundRequested;
+        this.roundResults = roundResults;
+        this.errorExpected = errorExpected;
+    }
+
+    @Test
+    public void test() {
+        Tournament t = new DummyTournament(
+                new TournamentData(
+                        TOURNAMENT_ID,
+                        NUMBER_OF_ROUNDS,
+                        new TournamentInput(Format.CONSTRUCTED_CASUAL, "", this.players),
+                        this.rounds));
+        try {
+            t.registerResults(this.roundRequested, this.roundResults);
+        } catch (Exception e) {
+            if (this.errorExpected != null) {
+                System.out.println(e);
+                Assert.assertEquals(this.errorExpected, e.getMessage());
+                return;
+            }
+            throw e;
+        }
+    }
+}
